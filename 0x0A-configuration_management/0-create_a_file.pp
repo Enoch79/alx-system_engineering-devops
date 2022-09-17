933bc922924270c51f961cd057ff922e9a76fbe8
Enoch79 <linanar8@gmail.com>
2022-09-16 17:48:54 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/pup
-# Create an file called 'school' on he dir tmp
+# Create an file called 'school' on the dir tmp
 file { 'school':
   ensure  => file,
   path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content => 'I love puppet'
+  content => 'I love Puppet'
 }
