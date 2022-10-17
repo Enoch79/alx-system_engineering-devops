a5e0abfacd99416ad6f7daf42faf811b4743462b
Enoch79 <linanar8@gmail.com>
2022-10-17 03:32:07 -0700

    ssh update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,13 @@
-# Set the config of a ssh_file
-file_line { 'Change the main private key':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
+# Configure ssh config file
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
 }
 
-file_line { 'No Authenticate with passowrd':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true
 }
